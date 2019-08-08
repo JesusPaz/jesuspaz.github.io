cfcb75e6020cf4e521f0707db065bcff1dc4d632
Jesus Paz <jeherpabo@gmail.com>
2019-08-08 07:22:12 -0500

    Change between skills (front and back) when the user click in the respective box

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-function show () {
-    document.getElementById("box").style.display = "block";
+function show_front () {
+    document.getElementById("box_front").style.display = "block";
+    document.getElementById("box_back").style.display= "none";
+
 }
 
-function hide () {
-    document.getElementById("box").style.display= "none";
+function hide_front () {
+    document.getElementById("box_front").style.display= "none";
 }
 
-function show_hide(){
-    var box = document.getElementById("box");
+function show_hide_front(){
+    var box = document.getElementById("box_front");
 
     if(box.style.display == "none"){
-        show();
+        show_front();
         window.scrollTo(0, 1100); 
     }else{
-        hide();
+        hide_front();
+    }
+}
+
+
+function show_back () {
+    document.getElementById("box_back").style.display = "block";
+    document.getElementById("box_front").style.display= "none";
+
+}
+
+function hide_back () {
+    document.getElementById("box_back").style.display= "none";
+}
+
+function show_hide_back(){
+    var box = document.getElementById("box_back");
+
+    if(box.style.display == "none"){
+        show_back();
+        window.scrollTo(0, 1100); 
+    }else{
+        hide_back();
     }
 }
